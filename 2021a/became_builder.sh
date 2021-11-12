0e206a889bb5f2d8912bb07721532aa37c37be53
Josef Dvoracek <jose@fzu.cz>
2021-11-12 18:29:45 +0100

    build in local scratch

--- 2021a/became_builder.sh
+++ 2021a/became_builder.sh
@@ -2,6 +2,8 @@
 
 export EASYBUILD_PREFIX=/home/soft/c8/2021a
 export MODULEPATH=$EASYBUILD_PREFIX/modules/all
+export EASYBUILD_BUILDPATH=/mnt/local-scratch/eb
+sudo mkdir -p ${EASYBUILD_BUILDPATH}
 module load tools/EasyBuild
 eb --install-latest-eb-release
 
