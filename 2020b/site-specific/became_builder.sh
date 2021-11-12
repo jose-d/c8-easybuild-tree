a5ba1090e14de6771c2982f63a52a2a344afa4ab
Josef Dvoracek <jose@fzu.cz>
2021-11-12 18:31:04 +0100

    build in scratch

--- 2020b/site-specific/became_builder.sh
+++ 2020b/site-specific/became_builder.sh
@@ -2,6 +2,7 @@
 
 export EASYBUILD_PREFIX=/home/soft/c8/2020b
 export MODULEPATH=$EASYBUILD_PREFIX/modules/all
+export EASYBUILD_BUILDPATH=/mnt/local-scratch/eb
 module load tools/EasyBuild
 eb --install-latest-eb-release
 
